b89e3ee518ec0473b9e349fd404bae1e87144005
Infixz <630572808@qq.com>
2015-01-23 10:57:37 +0800

    Done

--- postiteminfo.py
+++ postiteminfo.py
@@ -65,32 +65,37 @@ formlistfixed=["model",
 "sn2",
 "sn",
 "dnsname",
-"warrantymonths",
+"userid",
 "comments",
+"warrantymonths",
 "label"]
 
 #csv转化来的n个条目（每个条目一个formdict）&处理
-reader = csv.reader(file(r'C:\Users\johwan01\Documents\Johnathon wang\ARM\asset_info.csv', 'rb'))
+reader = csv.reader(file(r'C:\Users\johwan01\Documents\GitHub\ARM-ITDM-MANAGEMENT\userlist.csv', 'rb'))
+usernamedict={}
+for line in reader:  #formlistval毛坯
+    usernamedict[line[1]]=line[0]
+reader = csv.reader(file(r'C:\Users\johwan01\Documents\GitHub\ARM-ITDM-MANAGEMENT\item_info.csv', 'rb'))
 formlistval=[line for line in reader]  #formlistval毛坯
 for i in range(len(formlistval)):#formlistval处理(合并备注，添加标题)
-    formlistval[i][1]+='(P/N)'
-    formlistval[i][5]='username:'+formlistval[i][5]+"\nDepartment:"+formlistval[i][6]
-    formlistval[i].pop(6)
-    formlistval[i][6]+='(Asset Id)'
+    formlistval[i][1]='P/N:'+formlistval[i][1]
     try:
-        formlistval[i][4]=int(formlistval[i][4][5])-2
+        formlistval[i][4]=int(usernamedict[formlistval[i][4]])
     except:
-        continue
+        formlistval[i][4]=1
+    formlistval[i][5]="\nDepartment:"+formlistval[i][5]
+    formlistval[i][7]="Asset_Id:"+formlistval[i][7]
 
 #dict更新函数
-def dictupdate(x):#用来n次刷新_需要更新的7个元素的字典
-    for i in range(7):  #6=len(formlistfixed)
+def dictupdate(x):#用来x次刷新需要更新的len(formlistfixed)个元素的字典
+    for i in range(len(formlistfixed)):
         formdict[formlistfixed[i]]=formlistval[x][i]
 
 #网络端初始化
 login_page = "http://10.164.1.100/index.php"
-edit_page = "http://10.164.1.100/index.php?action=edititem&id=new"
 login_data = {"authusername":"Jonathan","authpassword":"1234"}
+itemsedit_page = "http://10.164.1.100/index.php?action=edititem&id=new"
+
 
 #构造opener
 handlers = [StreamingHTTPHandler, StreamingHTTPRedirectHandler, StreamingHTTPSHandler]
@@ -102,12 +107,12 @@ urllib2.install_opener(opener)
 #</初始化>
 
 #登陆及提交表单
-opener.open(login_page,urllib.urlencode(login_data))#登陆；存疑：要request才能算请求页面么；应该没事，记得上次返回页面已经是itemedit页面了
-opener.open(edit_page)
+opener.open(login_page,urllib.urlencode(login_data))#登陆；存疑：要request才能算请求页面么；应该没事，记得上次返回页面已经是itemedit页面了；最终——没问题
+opener.open(itemsedit_page)
 for n in range(len(formlistval)):#n次迭代发送完整个csv文件的item
     dictupdate(n)#多少次发送多少次item_dict更新
-    datagen, headers = multipart_encode(formdict)
-    request = urllib2.Request(edit_page, datagen, headers)
+    datagen, headers = multipart_encode(formdict)#对更新过的dict进行编码
+    request = urllib2.Request(itemsedit_page, datagen, headers)
     result = urllib2.urlopen(request)
     print r"There's %s items last."% (len(formlistval)-n)
 print u'\nIt has been done!'
